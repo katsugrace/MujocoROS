01863fb7779965ca3f9f7403e57e23bf05c65604
katsugrace <victor.drobizov@gmail.com>
2025-03-21 21:18:07 +0500

    created ros2 package

---
+++ kg_mujoco_ros/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+PACKAGE_NAME = 'kg_mujoco_ros'
+
+setup(
+    name=PACKAGE_NAME,
+    version='0.0.0',
+    data_files=[
+        ('share/' + PACKAGE_NAME, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Victor',
+    maintainer_email='victor.drobizov@gmail.com',
+    description='Mujoco ROS2',
+    license='Apache License 2.0',
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
